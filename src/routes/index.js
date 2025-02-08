233aec1968b672c52efdf313060f4e5128080326
Nguyen Tri Khanh <khanh_dth215962@student.agu.edu.vn>
2025-02-08 19:39:05 +0700

    install prettier lint-lintstaged husky

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+function route(app) {
+  // app.get('/', (req, res) => {
+  //     res.render('home')
+  // });
+  // app.get('/news', (req, res) => {
+  //     res.render('news')
+  // });
+  app.use('/news', newsRouter);
+  // app.get('/search', (req, res) => {
+  //     // console.log(req.query.q)
+  //     res.render('search')
+  // })
+  // app.post('/search', (req, res) => {
+  //     console.log(req.body)
+  //     res.send('')
+  // })
+
+  app.use('/', siteRouter);
+}
+
+module.exports = route;
